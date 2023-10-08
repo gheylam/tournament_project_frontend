21b603f87218d9ed094ebd69692ae820c65a8cb1
ghey <tsz@zeropointmotion.com>
2023-10-08 22:34:11 +0100

    :sparkles: endpoints shown in homepage

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,16 @@ function Home() {
     return (
         <div>
             <h1>Round Up - Homepage</h1>
-        </div>
+            <h2>Available endpoints</h2>
+            <ul>
+                <li>Home page - "/" </li>
+                <li>Create tournament - "/create-tournament"</li>
+                <li>Owner lobby - "/owner-lobby/:key"</li>
+                <li>Join - "/join/:key"</li>
+                <li>Joiner lobby - "/joiner-lobby/:key"</li>
+                <li>Brackets - "/tournament/:key"</li>
+            </ul>
+        </div >
     );
 }
 
