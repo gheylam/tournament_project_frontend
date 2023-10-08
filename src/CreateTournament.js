08aa8d314e135f47c4c9bde64af5bc3ba72a269c
ghey <tsz@zeropointmotion.com>
2023-10-08 18:05:39 +0100

    :sparkles: mostly done, apart from blue side button

--- src/CreateTournament.js
+++ src/CreateTournament.js
@@ -1,10 +1,14 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './fonts.css';
+import './CreateTournament.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+
+import buttonArrow from './images/button_arrow.svg'; // Import the arrow image
 
 const ENDPOINT = "http://localhost:8000/api/create-tournament"
 
@@ -40,30 +44,46 @@ function CreateTournament() {
     };
 
     return (
-        <div>
-            <h1>Round Up</h1>
-            <h2>Create tournament</h2>
-            <form>
-                <label>Name of tournament:
-                    <input type="text" onChange={(e) => setName(e.target.value)} />
-                </label>
-            </form>
-            <Form>
-                <Form.Check
-                    inline
-                    type="switch"
-                    id="custom-switch"
-                    label="Public"
-                />
-                <Form.Select aria-label="Team size select" onChange={(e) => setNumOfTeams(e.target.value)}>
-                    <option>Select number of Teams</option>
-                    <option value="2">Two</option>
-                    <option value="4">Four</option>
-                    <option value="8">Eight</option>
-                    <option value="16">Sixteen</option>
-                </Form.Select>
-            </Form>
-            <Button variant="primary" onClick={sendPostRequest}>Create</Button>
+        <div className="main-frame">
+            
+            <div className="logo-banner">
+                <p className="roundup">
+                    <span className="text-wrapper">Round</span>
+                    <span className="span">up</span>
+                </p>
+            </div>
+
+            <div className="heading-banner">
+                <div className="text-wrapper">
+                    Create a tournament
+                </div>
+            </div>
+
+            <div className="form-frame">
+                <div className="div-01">
+                    <div className="text-wrapper">Name of tournament</div>
+                    <input className="textinput" type="text" onChange={(e) => setName(e.target.value)} />
+                </div>
+
+                <div className="div-02">
+                    <div className="text-wrapper">Number of teams</div>
+                    <Form>
+                        <Form.Select className="select" aria-label="Team size select" onChange={(e) => setNumOfTeams(e.target.value)}>
+                            <option>Select number of Teams</option>
+                            <option value="2">Two</option>
+                            <option value="4">Four</option>
+                            <option value="8">Eight</option>
+                            <option value="16">Sixteen</option>
+                        </Form.Select>
+                    </Form>
+                </div>
+            </div>
+            <div className='footer-frame'>
+                <Button className="button" variant="primary" onClick={sendPostRequest}>
+                    <span className="button-text">Create tournament</span>
+                    <img src={buttonArrow} alt="Arrow" className="button-arrow" />
+                </Button>
+            </div>
         </div>
     );
 }
